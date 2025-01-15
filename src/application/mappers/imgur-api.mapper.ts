c2dbe9e8e886d7677f40fbda513b50d77a5b57d0
Dominic <string.reverse@proton.me>
2025-01-15 18:48:07 -0500

    Created mapper for imgur api

---
+++ src/application/mappers/imgur-api.mapper.ts
@@ -0,0 +1,38 @@
+import { AlbumModel } from "@/application/models/album.model";
+import { ImageModel } from "@/application/models/image.model";
+
+export function dtoToAlbumModel(dto: any) {
+  if (!dto) {
+    throw new Error('Invalid DTO.');
+  }
+
+  try {
+    return new AlbumModel(
+      dto.id,
+      dto.title,
+      dto.cover,
+      (dto.images || []).map((imageDto: any) => dtoToImageModel(imageDto)),
+    )
+  } catch (err) {
+    console.error('Error mapping DTO to Album Model:', err);
+    throw err;
+  }
+}
+
+export function dtoToImageModel(dto: any) {
+  if (!dto) {
+    throw new Error('Invalid DTO.');
+  }
+
+  try {
+    return new ImageModel(
+      dto.id,
+      dto.title,
+      dto.description,
+      dto.link,
+    )
+  } catch (err) {
+    console.error('Error mapping DTO to Image Model:', err);
+    throw err;
+  }
+}
