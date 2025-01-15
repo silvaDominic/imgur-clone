35d073fc1fbb9cb5fb6be4c8f0f5d1a5841a8606
Dominic <string.reverse@proton.me>
2025-01-15 18:03:56 -0500

    Created primitive template for  app comp

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+
 export function App() {
+  const [searchTerm, setSearchTerm] = useState<string>('');
+
   return (
-    <div>
-      Imgur Clone
+    <div className="app">
+      <main>
+        <h1>Imgur Search</h1>
+
+        <div id="search-container">
+          <input placeholder='Search albums' type="text" onChange={(e) => setSearchTerm(e.target.value)}/>
+
+          <div>
+            {searchTerm}
+            {/* LOADING TEMPLATE */}
+            {/* GRID */}
+          </div>
+        </div>
+
+
+      </main>
     </div>
-  )
+  );
 }
