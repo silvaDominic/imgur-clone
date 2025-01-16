59e8585d629551a40587ff0fd835b7de9f5cfa45
Dominic <string.reverse@proton.me>
2025-01-16 00:45:43 -0500

    Create views directory and 'album search' view

--- src/presentation/app.tsx
+++ src/presentation/app.tsx
@@ -1,59 +1,11 @@
-import { useEffect, useState } from "react";
-import { useSearch } from "@/presentation/hooks/use-search.hook";
-import { AlbumModel } from "@/application/models/album.model";
-
 import './app.styles.css';
-import { GridItem } from "@/presentation/components/grid/grid-item";
-import { Grid } from "@/presentation/components/grid/grid";
-import { AlbumLink } from "@/presentation/components/album-link/album-link";
-import { LoadingWidget } from "@/presentation/components/loading-widget/loading-widget";
+import { AlbumSearchView } from "@/presentation/views/album-search/album-search.view";
 
 export function App() {
-  const [searchTerm, setSearchTerm] = useState<string>('');
-
-  const {
-    data,
-    isLoading,
-    error,
-    searchByAlbum
-  } = useSearch();
-
-  useEffect(() => {
-    let timeout = 0;
-    if (searchTerm) {
-      timeout = setTimeout(() => searchByAlbum(searchTerm), 500);
-    }
-
-    return () => clearTimeout(timeout);
-  }, [searchTerm]);
-
   return (
     <div className="app">
       <main>
-        <h1>Imgur Search</h1>
-
-        <div id="search-container">
-          <input placeholder='Search albums' type="text" onChange={(e) => setSearchTerm(e.target.value)}/>
-
-          <div>
-            {
-              isLoading
-                ? <LoadingWidget isLoading={isLoading} />
-                : <Grid>
-                  {data.map((album: AlbumModel) => (
-                    <GridItem key={album.id}>
-                      <AlbumLink
-                        id={album.id}
-                        imgSrc={`https://i.imgur.com/${album.coverImageId}.jpeg`}
-                        imgAltText={album.title}
-                        images={album.images}
-                      />
-                    </GridItem>
-                  ))}
-                </Grid>
-            }
-          </div>
-        </div>
+        <AlbumSearchView />
       </main>
     </div>
   );
---
+++ src/presentation/views/album-search/album-search.view.tsx
@@ -0,0 +1,58 @@
+import { LoadingWidget } from "@/presentation/components/loading-widget/loading-widget";
+import { Grid } from "@/presentation/components/grid/grid";
+import { AlbumModel } from "@/application/models/album.model";
+import { GridItem } from "@/presentation/components/grid/grid-item";
+import { AlbumLink } from "@/presentation/components/album-link/album-link";
+import { ReactElement, useEffect, useState } from "react";
+import { useSearch } from "@/presentation/hooks/use-search.hook";
+
+import './album-search.styles.css';
+
+export function AlbumSearchView(): ReactElement {
+  const [searchTerm, setSearchTerm] = useState<string>('');
+
+  const {
+    data,
+    isLoading,
+    error,
+    searchByAlbum
+  } = useSearch();
+
+  useEffect(() => {
+    let timeout = 0;
+    if (searchTerm) {
+      timeout = setTimeout(() => searchByAlbum(searchTerm), 500);
+    }
+
+    return () => clearTimeout(timeout);
+  }, [searchTerm]);
+
+  return (
+    <div>
+      <h1>Imgur Search</h1>
+
+      <div id="search-container">
+        <input placeholder='Search albums' type="text" onChange={(e) => setSearchTerm(e.target.value)}/>
+
+        <div>
+          {
+            isLoading
+              ? <LoadingWidget isLoading={isLoading}/>
+              : <Grid>
+                {data.map((album: AlbumModel) => (
+                  <GridItem key={album.id}>
+                    <AlbumLink
+                      id={album.id}
+                      imgSrc={`https://i.imgur.com/${album.coverImageId}.jpeg`}
+                      imgAltText={album.title}
+                      images={album.images}
+                    />
+                  </GridItem>
+                ))}
+              </Grid>
+          }
+        </div>
+      </div>
+    </div>
+  );
+}
